8a249abfb8dc80e228e33dedff62c865b163c179
Jacob Schellenberg <34260365+jacobschellenberg@users.noreply.github.com>
2025-08-02 17:48:38 -0700

    Update directory.

--- Setup.bat
+++ Setup.bat
@@ -3,16 +3,15 @@ SETLOCAL
 
 REM === Configuration ===
 set SCRIPT_DIR=%~dp0
-set SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
 set SCHTASKS="C:\Windows\System32\schtasks.exe"
 
 REM === AddSpeedLimit Task ===
 echo Importing AddSpeedLimit task...
-%SCHTASKS% /Create /TN "SyncthingScripts_AddSpeedLimit" /XML "%SCRIPT_DIR%AddSpeedLimit.xml" /F
+%SCHTASKS% /Create /TN "SyncthingScripts_AddSpeedLimit" /XML "%SCRIPT_DIR%\AddSpeedLimit.xml" /F
 
 REM === RemoveSpeedLimit Task ===
 echo Importing RemoveSpeedLimit task...
-%SCHTASKS% /Create /TN "SyncthingScripts_RemoveSpeedLimit" /XML "%SCRIPT_DIR%RemoveSpeedLimit.xml" /F
+%SCHTASKS% /Create /TN "SyncthingScripts_RemoveSpeedLimit" /XML "%SCRIPT_DIR%\RemoveSpeedLimit.xml" /F
 
 echo.
 echo Syncthing bandwidth automation tasks installed successfully.
