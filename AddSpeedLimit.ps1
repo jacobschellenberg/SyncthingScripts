288a04200703d488fe94d389e876c57097568973
Jacob Schellenberg <34260365+jacobschellenberg@users.noreply.github.com>
2025-08-02 19:51:32 -0700

    Add application/json, use basic parsing.

--- AddSpeedLimit.ps1
+++ AddSpeedLimit.ps1
@@ -6,13 +6,16 @@ $baseUrl = $config.host
 $rateIn = $config.rateIn
 $rateOut = $config.rateOut
 
-$headers = @{ "X-API-Key" = $apiKey }
+$headers = @{
+    "X-API-Key" = $apiKey
+    "Accept" = "application/json"
+}
 
 $configJson = '_temp_syncthing_config.json'
 $modifiedJson = '_temp_modified_config.json'
 
 Write-Host "Fetching current Syncthing system config..."
-Invoke-RestMethod -Uri "$baseUrl/rest/system/config" -Headers $headers -OutFile $configJson
+Invoke-RestMethod -Uri "$baseUrl/rest/system/config" -Headers $headers -OutFile $configJson -UseBasicParsing
 
 Write-Host "Modifying system config to apply global rate limits..."
 $json = Get-Content $configJson -Raw | ConvertFrom-Json
@@ -21,7 +24,7 @@ $json.options.maxSendKbps = $rateOut
 $json | ConvertTo-Json -Depth 10 | Set-Content -Encoding UTF8 $modifiedJson
 
 Write-Host "Sending updated system config back to Syncthing..."
-Invoke-RestMethod -Uri "$baseUrl/rest/system/config" -Method Put -Headers $headers -ContentType 'application/json' -InFile $modifiedJson
+Invoke-RestMethod -Uri "$baseUrl/rest/system/config" -Method Put -Headers $headers -ContentType 'application/json' -InFile $modifiedJson -UseBasicParsing
 
 Write-Host "Done."
 
--- RemoveSpeedLimit.ps1
+++ RemoveSpeedLimit.ps1
@@ -7,10 +7,13 @@ $baseUrl = $config.host
 $configJson = '_temp_syncthing_config.json'
 $modifiedJson = '_temp_modified_config.json'
 
-$headers = @{ "X-API-Key" = $apiKey }
+$headers = @{
+    "X-API-Key" = $apiKey
+    "Accept" = "application/json"
+}
 
 Write-Host "Fetching current Syncthing config..."
-Invoke-RestMethod -Uri "$baseUrl/rest/system/config" -Headers $headers -OutFile $configJson
+Invoke-RestMethod -Uri "$baseUrl/rest/system/config" -Headers $headers -OutFile $configJson -UseBasicParsing
 
 Write-Host "Modifying config to remove rate limits..."
 $json = Get-Content $configJson -Raw | ConvertFrom-Json
@@ -19,7 +22,7 @@ $json.options.maxRecvKbps = 0
 $json | ConvertTo-Json -Depth 10 | Set-Content -Encoding UTF8 $modifiedJson
 
 Write-Host "Sending updated config back to Syncthing..."
-Invoke-RestMethod -Uri "$baseUrl/rest/system/config" -Method Put -Headers $headers -ContentType 'application/json' -InFile $modifiedJson
+Invoke-RestMethod -Uri "$baseUrl/rest/system/config" -Method Put -Headers $headers -ContentType 'application/json' -InFile $modifiedJson -UseBasicParsing
 
 Write-Host "Done."
 
