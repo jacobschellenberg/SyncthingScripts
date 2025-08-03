b4601aeff201cad7e4ff0f8c60dfbb33269f43f7
Jacob Schellenberg <34260365+jacobschellenberg@users.noreply.github.com>
2025-08-02 17:43:27 -0700

    Fix file path.

--- Setup.bat
+++ Setup.bat
@@ -8,11 +8,11 @@ set SCHTASKS="C:\Windows\System32\schtasks.exe"
 
 REM === AddSpeedLimit Task ===
 echo Importing AddSpeedLimit task...
-%SCHTASKS% /Create /TN "Syncthing\SyncthingScripts_AddSpeedLimit" /XML "%SCRIPT_DIR%AddSpeedLimit.xml" /F
+%SCHTASKS% /Create /TN "SyncthingScripts_AddSpeedLimit" /XML "%SCRIPT_DIR%AddSpeedLimit.xml" /F
 
 REM === RemoveSpeedLimit Task ===
 echo Importing RemoveSpeedLimit task...
-%SCHTASKS% /Create /TN "Syncthing\SyncthingScripts_RemoveSpeedLimit" /XML "%SCRIPT_DIR%RemoveSpeedLimit.xml" /F
+%SCHTASKS% /Create /TN "SyncthingScripts_RemoveSpeedLimit" /XML "%SCRIPT_DIR%RemoveSpeedLimit.xml" /F
 
 echo.
 echo Syncthing bandwidth automation tasks installed successfully.
