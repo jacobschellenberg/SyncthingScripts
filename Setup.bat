b0aaf24af611f1f64b1ffd653e2b376fa1c32083
Jacob Schellenberg <34260365+jacobschellenberg@users.noreply.github.com>
2025-08-02 17:50:00 -0700

    Add debugging.

--- Setup.bat
+++ Setup.bat
@@ -3,14 +3,17 @@ SETLOCAL
 
 REM === Configuration ===
 set SCRIPT_DIR=%~dp0
+echo SCRIPT_DIR is: %SCRIPT_DIR%
 set SCHTASKS="C:\Windows\System32\schtasks.exe"
 
 REM === AddSpeedLimit Task ===
 echo Importing AddSpeedLimit task...
+echo Running: %SCHTASKS% /Create /TN "SyncthingScripts_AddSpeedLimit" /XML "%SCRIPT_DIR%\AddSpeedLimit.xml" /F
 %SCHTASKS% /Create /TN "SyncthingScripts_AddSpeedLimit" /XML "%SCRIPT_DIR%\AddSpeedLimit.xml" /F
 
 REM === RemoveSpeedLimit Task ===
 echo Importing RemoveSpeedLimit task...
+echo Running: %SCHTASKS% /Create /TN "SyncthingScripts_RemoveSpeedLimit" /XML "%SCRIPT_DIR%\RemoveSpeedLimit.xml" /F
 %SCHTASKS% /Create /TN "SyncthingScripts_RemoveSpeedLimit" /XML "%SCRIPT_DIR%\RemoveSpeedLimit.xml" /F
 
 echo.
